a61e7078d83c78f98295c23d38bcbb38e219d65e
marmin74 <55284207+marmin74@users.noreply.github.com>
2025-05-08 23:19:58 +0200

    moduli

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
-module.exports = { reactStrictMode: true };
+// next.config.js
+module.exports = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          'utf-8-validate': false,
+          'bufferutil': false,
+        };
+      }
+      return config;
+    },
+  }
+  
