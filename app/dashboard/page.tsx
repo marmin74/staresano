7eb90dfb6403e5089afb74379f061d958e07efb9
marmin74 <55284207+marmin74@users.noreply.github.com>
2025-05-09 00:01:37 +0200

    dash

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,9 +2,10 @@
 import { useEffect, useState } from 'react';
 import { supabase } from '../../lib/supabaseClient';
 import { useRouter } from 'next/navigation';
+import { User } from '@supabase/supabase-js';
 
 export default function Dashboard() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<User | null>(null);
   const router = useRouter();
 
   useEffect(() => {
